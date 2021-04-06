4838ab604f70f2d8081a60e3587ded6f40c3e8bb
Abhishek262 <abhishek262014@gmail.com>
2021-04-06 15:56:23 +0530

    Added skeleton code

---
+++ kvstore/src/server/kvcacheset.c
@@ -0,0 +1,46 @@
+#include <pthread.h>
+#include <errno.h>
+#include <stdbool.h>
+#include "uthash.h"
+#include "utlist.h"
+#include "kvconstants.h"
+#include "kvcacheset.h"
+
+/* Initializes CACHESET to hold a maximum of ELEM_PER_SET elements.
+ * ELEM_PER_SET must be at least 2.
+ * Returns 0 if successful, else a negative error code. */
+int kvcacheset_init(kvcacheset_t *cacheset, unsigned int elem_per_set) {
+  int ret;
+  if (elem_per_set < 2)
+    return -1;
+  cacheset->elem_per_set = elem_per_set;
+  if ((ret = pthread_rwlock_init(&cacheset->lock, NULL)) < 0)
+    return ret;
+  cacheset->num_entries = 0;
+  return 0;
+}
+
+
+/* Get the entry corresponding to KEY from CACHESET. Returns 0 if successful,
+ * else returns a negative error code. If successful, populates VALUE with a
+ * malloced string which should later be freed. */
+int kvcacheset_get(kvcacheset_t *cacheset, char *key, char **value) {
+  return -1;
+}
+
+/* Add the given KEY, VALUE pair to CACHESET. Returns 0 if successful, else
+ * returns a negative error code. Should evict elements if necessary to not
+ * exceed CACHESET->elem_per_set total entries. */
+int kvcacheset_put(kvcacheset_t *cacheset, char *key, char *value) {
+  return -1;
+}
+
+/* Deletes the entry corresponding to KEY from CACHESET. Returns 0 if
+ * successful, else returns a negative error code. */
+int kvcacheset_del(kvcacheset_t *cacheset, char *key) {
+  return -1;
+}
+
+/* Completely clears this cache set. For testing purposes. */
+void kvcacheset_clear(kvcacheset_t *cacheset) {
+}
