8209a3c8885c7ee847001df7306dba0bfeaae59d
Abhishek262 <abhishek262014@gmail.com>
2021-04-27 18:45:30 +0530

    kvslave  added argument parser

--- kvstore_py/src/server/kvslave.py
+++ kvstore_py/src/server/kvslave.py
@@ -8,13 +8,35 @@ usage  = "sage: kvslave [-t] [--tpc] [slave_port (default=9000)] [master_port (d
 tpcMode = 0
 slavePort = 9000
 masterPort  = 888
+mode = ""
 
 slaveHostName = "localhost"
 masterHostName = "localhost"
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 
-parser.add_argument("-t")
+parser.add_argument("-t",action="store_true",default=False)
+parser.add_argument("slave_port",type=int)
+parser.add_argument("master_port",type=int)
+args  = parser.parse_args()
+# print(args)
+
+tpcMode = args.t 
+slavePort = args.slave_port
+masterPort = args.master_port
+print(tpcMode,slavePort,masterPort)
+
+if(tpcMode):
+    mode = "(tpc)"
+
+if(tpcMode):
+    print("Slave server",mode," started on",slavePort," listening for master at ", masterHostName, " :",masterPort ) 
+else:
+    print("Single Node server started on port ",slavePort)
+
+
+
+
 
 
 
