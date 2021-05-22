ce84db2e123819e53913e5c3a915b7add6df852c
Abhishek262 <abhishek262014@gmail.com>
2021-05-22 17:18:21 +0530

    changed run_multiple_clients script

--- kvstore_py/src/client/run_multiple_clients.sh
+++ kvstore_py/src/client/run_multiple_clients.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-./interactive_client -p 8888 < command_list_small > cl1&
-./interactive_client -p 8888 < command_list_small > cl2&
-sudo lsof -i -P -n | grep LISTEN
+./interactive_client -p 8888 < command_list_small1 > cl1&
+./interactive_client -p 8888 < command_list_small2 > cl2&
