ce56d73633f7a6ea864386e79080ce3285e9b800
ishi20 <iagarwal20@gmail.com>
2021-05-22 20:07:22 +0530

    More results

--- kvstore_py/src/client/run_multiple_clients.sh
+++ kvstore_py/src/client/run_multiple_clients.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-./interactive_client -p 8888 < command_list_put &
+./interactive_client -p 8888 < command_list_put 
 rm results
 # ./interactive_client -p 8888 < command_list &
 
+./interactive_client -p 8888 < command_list &
 ./interactive_client -p 8888 < command_list &
 ./interactive_client -p 8888 < command_list &
 # ./interactive_client -p 8888 < command_list &
